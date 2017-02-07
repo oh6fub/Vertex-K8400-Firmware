30d5ec85053fdbc3d9869b8db54dba1af203aafb
Petri Pirttinen <petri.pirttinen@gmail.com>
2017-02-07 18:24:49 +0200

    Version M1-V1.4-H1/H2

--- H2/Marlin/planner.h
+++ H2/Marlin/planner.h
@@ -16,7 +16,7 @@
 
   You should have received a copy of the GNU General Public License
   along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
-*/
+*/ 
 
 // This module is to be considered a sub-module of stepper.c. Please don't include 
 // this file from any other module.
